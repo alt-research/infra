80d40f9296153e8c12ed0ac4f1db59461fab77fb
Aten Alt <aten@altresear.ch>
2025-07-17 15:04:23 +0800

    add logic to use dns rather than peerTlsInfo.LeafCertificate.DNSNames[0]

--- op-signer/service/auth.go
+++ op-signer/service/auth.go
@@ -3,7 +3,9 @@ package service
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
+	"strings"
 
 	oprpc "github.com/ethereum-optimism/optimism/op-service/rpc"
 	optls "github.com/ethereum-optimism/optimism/op-service/tls"
@@ -32,13 +34,34 @@ func NewAuthMiddleware() oprpc.Middleware {
 				return
 			}
 
-			clientInfo.ClientName = peerTlsInfo.LeafCertificate.DNSNames[0]
-			fmt.Println("request:", "r-header", r.Header, "url", r.URL, "host", r.Host, "tlsinfo0-dnsnames", peerTlsInfo.LeafCertificate.DNSNames)
+			//clientInfo.ClientName = peerTlsInfo.LeafCertificate.DNSNames[0]
+			dns := extractHostname(r.Host)
+			fmt.Println("request:", "r-header", r.Header, "url", r.URL, "host", r.Host, "dns", dns, "tlsinfo0-dnsnames", peerTlsInfo.LeafCertificate.DNSNames)
+			for _, name := range peerTlsInfo.LeafCertificate.DNSNames {
+				if name == dns {
+					clientInfo.ClientName = dns
+				}
+			}
+			if clientInfo.ClientName == "" {
+				s := fmt.Sprintf("client certificate provided but not in DNS SAN extension. current dns %s, DNSNames in certificate are: %s", dns, peerTlsInfo.LeafCertificate.DNSNames)
+				http.Error(w, s, 401)
+				return
+			}
 
 			ctx := context.WithValue(r.Context(), clientInfoContextKey{}, clientInfo)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
+}
+
+func extractHostname(host string) string {
+	if strings.Contains(host, ":") {
+		h, _, err := net.SplitHostPort(host)
+		if err == nil {
+			return h
+		}
+	}
+	return host
 }
 
 func ClientInfoFromContext(ctx context.Context) ClientInfo {
