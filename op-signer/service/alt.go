3e9889c60086fdd207db6f369de7eedce7258e7b
fyInALT <fy@altresear.ch>
2026-02-13 11:13:33 +0800

    add alt_address rpc

--- op-signer/service/alt.go
+++ op-signer/service/alt.go
@@ -40,5 +40,32 @@ func (s *AltService) Publickey(ctx context.Context) (hexutil.Bytes, error) {
 		return nil, rpc.HTTPError{StatusCode: 500, Status: "Internal Server Error", Body: []byte("unmarshaled public key is nil")}
 	}
 
+	return hexutil.Bytes(publicKey), nil
+}
+
+// SignTransaction will sign the given transaction with the key configured for the authenticated client
+func (s *AltService) Address(ctx context.Context) (hexutil.Bytes, error) {
+	clientInfo := ClientInfoFromContext(ctx)
+
+	s.logger.Debug("getting public key for client", "client", clientInfo.ClientName)
+	authConfig, err := s.config.GetAuthConfigForClient(clientInfo.ClientName, nil)
+	if err != nil {
+		return nil, rpc.HTTPError{StatusCode: 403, Status: "Forbidden", Body: []byte(err.Error())}
+	}
+
+	publicKey, err := s.provider.GetPublicKey(ctx, authConfig.KeyName)
+	if err != nil {
+		return nil, rpc.HTTPError{StatusCode: 404, Status: "GetPublicKey Failed", Body: []byte(err.Error())}
+	}
+
+	key, err := crypto.UnmarshalPubkey(publicKey)
+	if err != nil {
+		return nil, rpc.HTTPError{StatusCode: 500, Status: "Internal Server Error", Body: []byte(err.Error())}
+	}
+
+	if key == nil {
+		return nil, rpc.HTTPError{StatusCode: 500, Status: "Internal Server Error", Body: []byte("unmarshaled public key is nil")}
+	}
+
 	return hexutil.Bytes(crypto.PubkeyToAddress(*key).Bytes()), nil
 }
